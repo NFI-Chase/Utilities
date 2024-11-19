b7a94193ce7959ec3ee6f907cedef131feb88162
Michael Strydom <strymichael@gmail.com>
2024-11-19 20:55:06 +0100

    Update Utilities_Home.py

--- Utilities_Home.py
+++ Utilities_Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(
    page_title="Evosoft Utilities",
-   page_icon="🔧",
+   page_icon=":house:",
    layout="wide",
    initial_sidebar_state="expanded",
 )
